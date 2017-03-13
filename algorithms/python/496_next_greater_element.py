e4ee7034291fbeda48efa0d1c617be8a20eb49bd
ruichao-factual <ruichao@factual.com>
2017-03-13 21:51:48 +0800

    Add another solution for 496 next greater element

--- algorithms/python/496_next_greater_element.py
+++ algorithms/python/496_next_greater_element.py
@@ -20,3 +20,23 @@ class Solution(object):
                 results.append(-1)
 
         return results
+
+
+# Solution 2
+
+class Solution(object):
+    def nextGreaterElement(self, findNums, nums):
+        """
+        :type findNums: List[int]
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        result_hash = {}
+        stack = []
+
+        for num in nums:
+            while stack and num > stack[-1]:
+                result_hash[stack.pop()] = num
+            stack.append(num)
+
+        return [result_hash.get(x, -1) for x in findNums]
